f6ef7111bef2f89734b72771d7b958e7be9dbb1f
knight.gao <knight.gao@tineco.com>
2022-04-26 17:22:21 +0800

    feat: 完成3057题

---
+++ question/3057-easy-push/template.ts
@@ -0,0 +1,5 @@
+type Push<T extends unknown[], U> = [...T, U]
+// extends 可以用来做判断 true false
+export type {
+    Push
+}
---
+++ question/3057-easy-push/test-cases.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect } from '@type-challenges/utils'
+import type { Push } from './template'
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
+
